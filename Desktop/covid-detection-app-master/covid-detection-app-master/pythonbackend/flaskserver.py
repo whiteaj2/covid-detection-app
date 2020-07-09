073fbac9307ce27c78dffb1ea2d98863053ed0a9
Andrew White <whiteaj2@rose-hulman.edu>
2020-07-09 11:23:34 -0400

    Again, adding untracked file

--- Desktop/covid-detection-app-master/covid-detection-app-master/pythonbackend/flaskserver.py
+++ Desktop/covid-detection-app-master/covid-detection-app-master/pythonbackend/flaskserver.py
@@ -8,8 +8,7 @@ import sys
 app = Flask(__name__)
 CORS(app)
 
-mydb = mysql.connector.connect(host="127.0.0.1", user="root", password="root", database="TEST")
-
+cnx = mysql.connector.connect(user="application", password="Covid1234@@", host="SG-MySQLDatabase-2698-master.servers.mongodirector.com", port=3306, database="testing")
 @app.route("/handleSearch", methods=["POST"])
 def handleSearch():
     jsonString = "{'key': 'value'}"
@@ -17,11 +16,32 @@ def handleSearch():
 
 @app.route("/handleSubmit", methods=["POST"])
 def handleSubmit():
-	data = request.data.decode("utf-8")
-	print(data)
+    data = json.loads(request.data.decode("utf-8"))
+    email = data["email"]
+    password = data["password"]
+    print(email)
+    print(password)
+    
+    return json.dumps("{}")
 
+@app.route("/handleSignup", methods=["POST"])
+def handleSignup():
+    data = json.loads(request.data.decode("utf-8"))
+    lname = data["lname"]
+    fname = data["fname"]
+    uname = data["uname"]
+    password = data["password"]
+    email = data["email"]
+    confirmEmail = data["confirmEmail"]
 
-	return json.dumps("{}")
+    if(email != confirmEmail):
+        return json.dumps("{}")
+
+    cursor = cnx.cursor()
+
+    queryString = "INERST INTO users VALUES (" + uname + "," + fname + "," + lname + "," + "testaddress," + "testphone," + email + ");" 
+    cursor.execute(queryString)
+    cursor.close()
 
 
 if __name__ == '__main__':
