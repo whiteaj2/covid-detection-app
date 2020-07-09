21d0dace34f0d39f1c011ab26938754666f5e262
Andrew White <whiteaj2@rose-hulman.edu>
2020-07-09 11:07:05 -0400

    Small amount of db connection code

--- Desktop/covid-detection-app-master/covid-detection-app-master/covid-app/src/components/Signup.js
+++ Desktop/covid-detection-app-master/covid-detection-app-master/covid-app/src/components/Signup.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react'
 
 export class Signup extends Component {
-    signup(){
-        alert("Signup confirmed")
+  constructor(props) {
+    super(props)
+  }
+
+    state = {
+      fname: "",
+      lname: "",
+      email: "",
+      confirmEmail: "",
+      password: "",
+      uname: ""
     }
+    signup = () => {
+        fetch("/handleSignup", {method: "POST", body: JSON.stringify({email: this.state.email,
+                                                                      password: this.state.password,
+                                                                      confirmEmail: this.state.confirmEmail,
+                                                                      fname: this.state.fname,
+                                                                      lname: this.state.lname,
+                                                                      uname: this.state.uname})}).then(res => {return res.json()});
+    }
+
+    updateFirstName = (event) => {this.setState({fname: event.target.value})};
+    updateLastName = (event) => {this.setState({lname: event.target.value})};
+    updateEmail = (event) => {this.setState({email: event.target.value})};
+    updateConfirmEmail = (event) => {this.setState({confirmEmail: event.target.value})};
+    updatePassword = (event) => {this.setState({password: event.target.value})};
+    updateUsername = (event) => {this.setState({uname: event.target.value})};
+
     render() {
         return (<form><br/><br/><br/>
             <h4>Sign Up</h4>
@@ -12,18 +37,18 @@ export class Signup extends Component {
                 <br/>
                 <center>
 
-               <label> First name: <input type="text" className="form"  placeholder="John"/></label> <br/><br/>
-               <label> Last name: <input type="text" className="form" placeholder="Smith" /> </label><br/><br/>
-               <label> Email: <input type="email" className="form" placeholder="abc@xyz.com" /> </label><br/><br/>
-               <label> Confirm email: <input type="email" className="form" placeholder="abc@xyz.com" /></label> <br/><br/>
-               <label> Password: <input type="password" className="form" placeholder="Password goes here"/></label> <br/><br/>
-                
+               <label> First name: <input type="text" className="form"  placeholder="John" onChange={this.updateFirstName} /></label> <br/><br/>
+               <label> Last name: <input type="text" className="form" placeholder="Smith" onChange={this.updateLastName} /> </label><br/><br/>
+               <label> Email: <input type="email" className="form" placeholder="abc@xyz.com" onChange={this.updateEmail} /> </label><br/><br/>
+               <label> Confirm email: <input type="email" className="form" placeholder="abc@xyz.com" onChange={this.updateConfirmEmail} /></label> <br/><br/>
+               <label> Password: <input type="password" className="form" placeholder="Password goes here" onChange={this.updatePassword}/></label> <br/><br/>
+               <label> Username: <input type="text" className="form" placeholder="username" onChange={this.updateUsername} /></label> <br/><br/> 
                 </center>
 
                 </div>
 
                 <button type="submit" className="btn btn-success" 
-                    onClick={()=>this.signup()}>
+                    onClick={this.signup}>
                         Submit</button>
             </div>
             </form>
